002d4dca139c58e29c4e307ea3563438cb043b33
steve-evian74 <steve.bailleul74@gmail.com>
2020-06-15 16:45:09 +0200

    modifier le code

--- main.py
+++ main.py
@@ -3,22 +3,16 @@ import sys
 
 def connection (hote, pseudo):
 
-    hote = "arrobe.fr"
+    hote = ""
     port = 666
 
     connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connexion_avec_serveur.connect((hote, port))
+    connexion_avec_serveur.connect((infoUser["addresse"], port))
+
+    connexion_avec_serveur.send(infoUser["pseudo"])
     print("Connexion établie avec le serveur sur le port {}".format(port))
 
-    msg_a_envoyer = b""
-    while msg_a_envoyer != b"fin":
-        msg_a_envoyer = input("> ")
-        # Peut planter si vous tapez des caractères spéciaux
-        msg_a_envoyer = msg_a_envoyer.encode()
-        # On envoie le message
-        connexion_avec_serveur.send(msg_a_envoyer)
-        msg_recu = connexion_avec_serveur.recv(1024)
-        print(msg_recu.decode()) # Là encore, peut planter s'il y a des accents
+
 
     print("Fermeture de la connexion")
     connexion_avec_serveur.close()
