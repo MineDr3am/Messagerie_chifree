e8e70f5ddd4743f46aa6e431f5cafe51171e9e72
steve-evian74 <steve.bailleul74@gmail.com>
2020-06-19 14:04:07 +0200

    modifier le code de RSA

--- RSA.py
+++ RSA.py
@@ -1,38 +1,39 @@
 from Crypto.PublicKey import RSA
 
 
-#creation d un couple de cles
+#creation d´un couple de clés
 key = RSA.generate(1024)
 
 #chiffrage
-public_key = key.publickey()
-enc_data = public_key.encrypt(b"""bonjour c'est un message secret""", 32)
+#public_key = key.publickey()
+#enc_data = public_key.encrypt(b"""bonjour c'est un message secret""", 32)
 
-#dechiffrage
-x = key.decrypt(enc_data)
-x = x.decode('utf-8')
+# dechiffrage
+# x = key.decrypt(enc_data)
+# x = x.decode('utf-8')
 
-#afficher ses cles:
-k = key.exportKey('PEM')
-p = key.publickey().exportKey('PEM')
+# afficher ses clés:
+# k = key.exportKey('PEM')
+# p = key.publickey().exportKey('PEM')
 
-#sauvegarder ses cles dans des fichiers:
-with open('private.priv','w') as kf:
-	kf.write(k.decode())
-	kf.close()
 
-with open('public.pub','w') as pf:
-	pf.write(p.decode())
-	pf.close()
-
-#importer des cles a partir d'un fichier
-with open('private.priv','r') as fk:
+#importer des clés à partir d'un fichier
+with open('/home/steve-evian74/.ssh/steve.priv','r') as fk:
 	priv = fk.read()
 	fk.close()
 
-with open('public.pub','r') as fp:
+with open('/home/steve-evian74/.ssh/steve.pub','r') as fp:
 	pub = fp.read()
 	fp.close()
 
 privat = RSA.importKey(priv)
 public = RSA.importKey(pub)
+
+
+public_key = public.publickey()
+enc_data = public_key.encrypt(b"""bonjour c'est un message secret""", 32)
+
+x = privat.decrypt(enc_data)
+x = x.decode('utf-8')
+
+print(x)
