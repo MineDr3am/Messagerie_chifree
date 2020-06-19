dbcf2accc674d6a42b58b184cec5cb0333aa9f0d
steve-evian74 <steve.bailleul74@gmail.com>
2020-06-19 19:03:05 +0200

    Rsa et main

--- RSA.py
+++ RSA.py
@@ -2,7 +2,7 @@ from Crypto.PublicKey import RSA
 
 
 #creation d´un couple de clés
-key = RSA.generate(1024)
+#key = RSA.generate(1024)
 
 #chiffrage
 #public_key = key.publickey()
@@ -18,7 +18,7 @@ key = RSA.generate(1024)
 
 
 #importer des clés à partir d'un fichier
-with open('/home/steve-evian74/.ssh/steve.priv','r') as fk:
+with open('/home/steve-evian74/.ssh/hugue.priv','r') as fk:
 	priv = fk.read()
 	fk.close()
 
@@ -30,10 +30,17 @@ privat = RSA.importKey(priv)
 public = RSA.importKey(pub)
 
 
+# Je crypte le message avec la clé public de Steve. 
 public_key = public.publickey()
 enc_data = public_key.encrypt(b"""bonjour c'est un message secret""", 32)
 
+RsaToSend = "[RSA|steve]" + str(enc_data)
+print(RsaToSend) 
+print(RsaToSend)
+#Je decrypte le message avec la clé privé de steve 
+print(type(enc_data))
 x = privat.decrypt(enc_data)
 x = x.decode('utf-8')
 
 print(x)
+
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 import socket
 import threading
 from Crypto.PublicKey import RSA
+from pprint import pprint
+import re
 
 class Recv(threading.Thread):
     socket = None
@@ -37,11 +39,21 @@ class Send(threading.Thread):
 
     def run(self):
         while True:
-            saisie = input("")
-            self.socket.sendall(bytes(saisie + "\r\n", 'utf-8'))
+            saisie = input("")    
+            texte = crypt(saisie ,infoUser["pseudoContact"], infoUser["pseudo"])
+            Auteur = find_between( texte, "{RSA|", "}" )
+            Message = find_between( texte, "{RSA|"+infoUser["pseudoContact"]+"}", "Fin" )
+            print(Auteur)
+            
+            
+            Messagedecrypt = decrypt(Auteur, Message , infoUser["pseudoContact"] , infoUser["pseudo"])
+            
+            print(MessageCrypt)
+            #message = decrypt(MessageRsaSend["Destination"], MessageRsaSend["MessageCrypt"], infoUser["pseudoContact"], infoUser["pseudo"])     
+            self.socket.sendall(bytes(Messagedecrypt + "\r\n", 'utf-8'))
             time.sleep(0.001)
 
-def connection(hote, pseudo, pseudoContact ):
+def connection(hote, pseudo ):
     print(hote)
     port = 666
 
@@ -49,9 +61,77 @@ def connection(hote, pseudo, pseudoContact ):
     canal.connect((hote, port))
 
     Send(canal, pseudo)
-    send(canal, pseudoContact)
     Recv(canal)
 
+def decrypt (Destination, MessageCHiffrement , pseudoContact , pseudo):
+    if pseudoContact == "steve" and pseudo == "hugue":
+        if(Destination == pseudoContact ):
+
+            with open('/home/steve-evian74/.ssh/steve.priv','r') as fp:
+                priv = fp.read()
+                fp.close()
+            
+            privat = RSA.importKey(priv)
+
+            x = privat.decrypt(tuple(MessageCHiffrement))
+            x = x.decode('utf-8')
+            return x
+
+
+    if pseudoContact == "hugue" and pseudo == "steve":
+        if(Destination == pseudoContact ):
+            with open('/home/steve-evian74/.ssh/hugue.priv','r') as fp:
+                priv = fp.read()
+                fp.close()
+            
+            privat = RSA.importKey(priv)
+
+            x = privat.decrypt(tuple(MessageCHiffrement))
+            x = x.decode('utf-8')
+            return x
+
+def crypt(saisie , pseudoContact , pseudo):
+
+    if pseudoContact == "steve" and pseudo == "hugue":
+
+        
+        with open('/home/steve-evian74/.ssh/steve.pub','r') as fp:
+            pub = fp.read()
+            fp.close()
+        
+        public = RSA.importKey(pub)
+
+        public_key = public.publickey()
+        enc_data = public_key.encrypt(b"""Bonjour ca va ? !""", 32)
+
+        #RsaToSend = "[RSA |"+ pseudo +"]"+ str(enc_data)
+
+        NameCrypto =  "RSA"
+        Destination = pseudoContact
+        MessageCrypt =  str(enc_data)
+        
+        return "{" + NameCrypto + "|" + Destination + "}" + MessageCrypt +" Fin"
+        
+        
+
+    if pseudoContact == "hugue" and pseudo == "steve":
+    
+
+        with open('/home/steve-evian74/.ssh/hugue.pub','r') as fp:
+            pub = fp.read()
+            fp.close()
+        
+        public = RSA.importKey(pub)
+
+        public_key = public.publickey()
+        enc_data = public_key.encrypt(b"""bonjour c'est un message secret""", 32)
+
+        #RsaToSend = "[RSA |"+ pseudo +"]"+ str(enc_data)
+        NameCrypto =  "RSA"
+        Destination = pseudoContact
+        MessageCrypt =  str(enc_data)
+
+        return "{" + NameCrypto + "|" + Destination + "}" + MessageCrypt + " Fin"
 
 def getArg():
     if len(sys.argv) >= 3:
@@ -74,15 +154,43 @@ def getArg():
         return -1
 
 
+
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+def find_between_r( s, first, last ):
+    try:
+        start = s.rindex( first ) + len( first )
+        end = s.rindex( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+
+
+
+
+NameCrypto =  ""
+Destination = ""
+MessageCrypt =  ""
+
 infoUser = {}
+MessageRsaSend = {}
 if not getArg() == -1:
     infoUser = {
         "addresse": getArg()[0],
         "pseudo": getArg()[1],
-        "pseudoContact" : getArg()[2]
+        "pseudoContact": getArg()[2]
     }
 if infoUser:
-    print("pseudo: {}, addresse: {} , pseudoContact: {} ".format(infoUser["addresse"],infoUser["pseudo"],infoUser["pseudoContact"])
 
-    connection(infoUser["addresse"], infoUser["pseudo"], infoUser["pseudoContact"])
+    print("pseudo: {}, addresse: {} , pseudoContact: {} ".format(infoUser["addresse"],infoUser["pseudo"],infoUser["pseudoContact"]))
+
+    connection(infoUser["addresse"], infoUser["pseudo"])
+    
 
